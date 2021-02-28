b9f417bff872f9275e90997021c148765213bbb1
amol <amol96sanap@gmail.com>
2021-02-28 09:25:18 +0530

    changes done

--- Project/resumeproject/serv/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def service(request):
-    context={'service':'active'}
-    return render(request,'serv/service.html',context)
--- gs14/course/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('cor/', views.learn_django),
-]
